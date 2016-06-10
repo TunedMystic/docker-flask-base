333ee962ed107fb60e7e8c5f22b6fe80585aad45
Sandeep Jadoonanan <jsanweb@gmail.com>
2016-06-10 01:48:22 -0400

    Add content_type headers to '/companies' url.

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 app = Flask(__name__)
@@ -10,25 +10,49 @@ appdata_dir = os.path.abspath('/var/lib/appdata')
 data_file_name = os.path.join(appdata_dir, 'companies.json')
 
 
-@app.route('/data/x')
+@app.route('/')
+def default_msg():
+    return 'hello'
+
+
+@app.route('/echo/<x>/')
+def root(x='megatron'):
+    print('\nekoing...\n\n')
+    return 'OMG! Its {}!\n'.format(x)
+
+
+@app.route('/companies/')
 def data():
     try:
         data_file = json.load(open(data_file_name, 'rt'))
     except IOError:
         print('tried to open {}'.format(data_file_name))
         data_file = {}
-    return json.dumps(data_file, indent=2)
+
+    return make_response(
+        jsonify(data_file),
+        200,
+        {
+            'Content-Type': 'application/json',
+            'X-Powered-By': 'Magic'
+        }
+    )
 
 
-@app.route('/')
-def default_msg():
-    return 'hello'
+@app.route('/cors/', methods=['GET'])
+def echo():
+    headers = {
+        # Set Headers (GENERAL)
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'Magic',
+        # Set Headers (CORS)
+        'Access-Control-Allow-Origin': "*",
+        'Access-Control-Allow-Methods': 'HEAD, GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin Accept Content-Type X-Requested-With X-CSRF-Token',
+    }
 
-
-@app.route('/<x>')
-def root(x='megatron'):
-    print('\n\nhello..\n\n')
-    return 'OMG! Its {}!\n'.format(x)
+    search_text = request.args.get('q', '').strip()
+    return make_response(jsonify(message=search_text), 200, headers)
 
 
 if __name__ == '__main__':
